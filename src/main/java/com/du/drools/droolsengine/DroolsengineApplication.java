72eacd3fdcabdd681145e04154cb54bf0fa7f98c
suganandan <suganandan@hotmail.com>
2023-01-10 12:27:45 +0530

    Update DroolsengineApplication.java

--- src/main/java/com/du/drools/droolsengine/DroolsengineApplication.java
+++ src/main/java/com/du/drools/droolsengine/DroolsengineApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.boot.WebApplicationType;
 
 @SpringBootApplication
 public class DroolsengineApplication {
