ff3d5b5413de3cb12f31a3a4ee3dbc62e952fc5b
suganandan <suganandan@hotmail.com>
2023-01-11 09:05:36 +0530

    Adding Manifest and code clean

--- src/main/java/com/du/drools/droolsengine/DroolsengineApplication.java
+++ src/main/java/com/du/drools/droolsengine/DroolsengineApplication.java
@@ -2,20 +2,19 @@ package com.du.drools.droolsengine;
 
 import org.kie.api.KieServices;
 import org.kie.api.runtime.KieContainer;
-import org.springframework.boot.SpringApplication;
+import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
-import org.springframework.boot.WebApplicationType;
 
 @SpringBootApplication
 public class DroolsengineApplication {
 
 	public static void main(String[] args) {
-		ConfigurableApplicationContext ctx = new 
-  SpringApplicationBuilder(DroolsengineApplication.class).web(WebApplicationType.NONE).run();
-ctx.close();
+		ConfigurableApplicationContext ctx = new SpringApplicationBuilder(DroolsengineApplication.class)
+				.web(WebApplicationType.NONE).run();
+		ctx.close();
 	}
 
 	@Bean
